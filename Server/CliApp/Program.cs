89fc386cf7772117d1b108b66e0ed95a517b2e8d
Fadumo11 <fadumohmed1011@outlook.com>
2025-09-14 20:07:33 +0200

    Initial commit - Assignment 1 & 2 (entities, repos, CLI)

---
+++ Server/CliApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using InMemoryRepositories;
+using RepositoryContracts;
+using Server.CliApp.UI;
+
+namespace Server.CliApp;
+
+class Program
+{
+    static void Main()
+    {
+        // In-memory repositories
+        IUserRepository userRepo = new InMemoryUserRepository();
+        IPostRepository postRepo = new InMemoryPostRepository();
+        ICommentRepository commentRepo = new InMemoryCommentRepository();
+
+        // Start CLI
+        var app = new Cli(userRepo, postRepo, commentRepo);
+        app.Run();
+    }
+}
